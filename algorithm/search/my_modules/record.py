ebea19031900b9aa4e36b0df7c1ffd1e6f89c0df
hkawabata <13412823+hkawabata@users.noreply.github.com>
2019-09-28 13:43:09 +0900

    update

--- algorithm/search/my_modules/record.py
+++ algorithm/search/my_modules/record.py
@@ -2,7 +2,10 @@ import numpy as np
 import random
 
 
-def get_unique_records(num):
-    data = [[key, key*10] for key in random.sample(range(num), num)]
+def get_unique_records(num, sort=False):
+    if not sort:
+        data = [[key, key*10] for key in random.sample(range(num), num)]
+    else:
+        data = [[key, key*10] for key in range(num)]
     return np.array(data)
 
